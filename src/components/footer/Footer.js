6f43c8a2d4b8f385b4758ba30bd5305c955c153e
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-07 00:32:01 +0300

    added header and icons

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,12 @@
 import './footer.scss';
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span> my dashbord</span>
+      <span>Maksym Burynskyi</span>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,31 @@
 import './navbar.scss';
 
 function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>My dashbord</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/3.jpg"
+            alt=""
+          />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
