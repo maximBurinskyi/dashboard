57df6d86345d5078ed668142ee584260dfa8c727
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-06 21:33:38 +0300

    pages and layout

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,7 @@
+import './footer.scss';
+
+function Footer() {
+  return <div className="footer">Footer</div>;
+}
+
+export default Footer;
---
+++ src/pages/users/Users.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './users.scss';
+
+function Users() {
+  return <div className="users">Users</div>;
+}
+
+export default Users;
