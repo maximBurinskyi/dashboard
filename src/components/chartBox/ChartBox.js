3b3bbb26567320b21472e41edbb52356505c3ce8
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-11 09:15:40 +0300

    ended top deals

---
+++ src/components/chartBox/ChartBox.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './chartBox.scss';
+
+function ChartBox() {
+  return <div className="chartBox">ChartBox</div>;
+}
+
+export default ChartBox;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './home.scss';
 import topBox from '../../components/topBox/TopBox';
 import TopBox from '../../components/topBox/TopBox';
+import ChartBox from '../../components/chartBox/ChartBox';
 
 function Home() {
   return (
@@ -9,10 +10,18 @@ function Home() {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">Boax2</div>
-      <div className="box box3">Boax3</div>
-      <div className="box box4">Boax4</div>
-      <div className="box box5">Boax5</div>
+      <div className="box box2">
+        <ChartBox />
+      </div>
+      <div className="box box3">
+        <ChartBox />
+      </div>
+      <div className="box box4">
+        <ChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox />
+      </div>
       <div className="box box6">Boax6</div>
       <div className="box box7">Boax7</div>
       <div className="box box8">Boax8</div>
