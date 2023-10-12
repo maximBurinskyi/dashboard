77e30816515e80125a46bec053e8843fa7eb6dd7
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-12 08:53:25 +0300

    finished home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,6 +12,8 @@ import {
   chartBoxUser,
 } from '../../data';
 import BarChartBox from '../../components/barChartBox/BarChartBox';
+import PieChartBox from '../../components/pieChartBox/PieChartBox';
+import BigChartBox from '../../components/bigChartBox/BigChartBox';
 
 function Home() {
   return (
@@ -25,14 +27,18 @@ function Home() {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4"></div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
       <div className="box box5">
         <ChartBox {...chartBoxConversion} />
       </div>
       <div className="box box6">
         <ChartBox {...chartBoxRevenue} />
       </div>
-      <div className="box box7">Boax7</div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
       <div className="box box8">
         <BarChartBox {...barChartBoxVisit} />
       </div>
