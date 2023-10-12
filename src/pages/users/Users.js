c1800764f2c3e252e0c0062e0b20ebeb8335ca8b
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-12 23:54:28 +0300

    MUI DataGrid  installed

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import DataTable from '../../components/dataTable/DataTable';
 import './users.scss';
 
 function Users() {
-  return <div className="users">Users</div>;
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable />
+    </div>
+  );
 }
 
 export default Users;
