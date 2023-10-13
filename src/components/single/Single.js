219a3bd501c7ac8003085fa8849c328eda36d293
maximBurinskyi <maxim.burinskyi@gmail.com>
2023-10-13 20:43:10 +0300

    add new item added and product and user pages

---
+++ src/components/single/Single.js
@@ -0,0 +1,18 @@
+import './single.scss';
+
+function Single() {
+  return (
+    <div className="single">
+      <div className="view">
+        <div className="info">
+          <div className="topInfo"></div>
+          <div className="details"></div>
+        </div>
+        <div className="chart"></div>
+      </div>
+      <div className="activities"></div>
+    </div>
+  );
+}
+
+export default Single;
---
+++ src/pages/user/User.js
@@ -0,0 +1,12 @@
+import Single from '../../components/single/Single';
+import './user.scss';
+
+function User() {
+  return (
+    <div className="user">
+      <Single />
+    </div>
+  );
+}
+
+export default User;
